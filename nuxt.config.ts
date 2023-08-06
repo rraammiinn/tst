a9017bbf9a64b6f730d5d7ca91345f74c3b61952
ramin <ramin.kishani.farahani@gmail.com>
2023-08-06 13:22:38 +0330

    1

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  runtimeConfig: {
+    // The private keys which are only available within server-side
+    apiSecret: '',
+    // Keys within public, will be also exposed to the client-side
+    public: {
+      secret: ''
+    }
+  }
 })
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,10 @@
 <template>
     <h1 style="color: royalblue;">
-        home...
+        home...{{ runtimeConfig.public.secret }}
     </h1>
 </template>
+
+<script setup>
+const runtimeConfig = useRuntimeConfig()
+
+</script>
