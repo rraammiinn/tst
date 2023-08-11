08698c4d45f1fbe56b015161b97da6d033c750b5
ramin <ramin.kishani.farahani@gmail.com>
2023-08-11 19:45:53 +0330

    4

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,5 +8,24 @@ export default defineNuxtConfig({
     public: {
       secret: process.env.secret
     }
+  },
+  build: {
+    transpile:
+      process.env.NODE_ENV === 'production'
+        ? [
+            'naive-ui',
+            'vueuc',
+            '@css-render/vue3-ssr',
+            '@juggle/resize-observer'
+          ]
+        : ['@juggle/resize-observer']
+  },
+  vite: {
+    optimizeDeps: {
+      include:
+        process.env.NODE_ENV === 'development'
+          ? ['naive-ui', 'vueuc', 'date-fns-tz/esm/formatInTimeZone']
+          : []
+    }
   }
 })
--- pages/index.vue
+++ pages/index.vue
@@ -2,9 +2,12 @@
     <h1 style="color: royalblue;">
         home... ===> {{ runtimeConfig.public.secret }}
     </h1>
+
+    <NSwitch/>
 </template>
 
 <script setup>
+import {NSwitch} from 'naive-ui'
 const runtimeConfig = useRuntimeConfig()
 
 </script>
